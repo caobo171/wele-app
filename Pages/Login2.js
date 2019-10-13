a60d9d1f1969580956bd6ca75eb7d24b52f5eb41
caobo171 <caobo1712000@gmail.com>
2019-10-13 10:57:22 +0700

    add facebook login

--- Pages/Login2.js
+++ Pages/Login2.js
@@ -1,25 +1,39 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View , Button} from 'react-native';
 import {LoginButton, AccessToken} from 'react-native-fbsdk';
+
+import { LoginManager } from 'react-native-fbsdk';
+import {firebase} from '@react-native-firebase/auth';
+ 
+
+
+ 
+
 
 export default class Login extends Component {
   render() {
     return (
       <View>
-        <LoginButton
-          onLoginFinished={(error, result) => {
-            if (error) {
-              console.log('login has error: ' + result.error);
-            } else if (result.isCancelled) {
-              console.log('login is cancelled.');
-            } else {
-              AccessToken.getCurrentAccessToken().then(data => {
-                console.log(data.accessToken.toString());
-              });
-            }
-          }}
-          onLogoutFinished={() => console.log('logout.')}
-        />
+        <Button title="Login with facebook " onPress= {async ()=>{
+          // Login with permissions
+          const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+          
+          if (result.isCancelled) {
+            throw new Error('User cancelled the login process');
+          }
+
+          const data = await AccessToken.getCurrentAccessToken();
+          
+          if (!data) {
+            throw new Error('Something went wrong obtaining access token');
+          }
+
+          const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+
+          const data2 = await firebase.auth().signInWithCredential(credential);
+
+          console.log('check data2', data2)
+        }}/>
       </View>
     );
   }
